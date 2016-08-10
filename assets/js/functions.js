54229ad0dc6491fbea9590195f036d7639cd7f86
ThatOneBro <mr.dcfarris@gmail.com>
2016-08-10 17:32:46 -0500

    Encapsulated all functions in anonymous functions

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,84 +1,114 @@
-$(document).ready(function(){
-    mobileNavToggle();
-    smoothScroll(350);
-    nodeJump();
-    nodeRetract();
-    
-});
+var scrollSpeed = 350; 
 
-function mobileNavToggle(){
-    var $mobileNav = $('.mobile-nav-home');
-    
-    $('.mobile-nav-toggle').on('click', function(){
-        $mobileNav.toggleClass('active');
-    });
-}
+/*------------------------------
+		Mobile Nav
+------------------------------*/
 
-function smoothScroll(duration){
-	$('a[href^="#"]').on('click', function(event){
-        
-        var $this = $(this),
-            $target = $($this.attr('href')),
-            $html = $('html, body');
-        
-
-	    if ($target.length > 0) {
-	        event.preventDefault();
-	        $html.animate({
-	            scrollTop: $target.parent().offset().top
-	        }, duration);
-	    }
+(function( $ ){
+	$(document).ready(function(){
+		var $mobileNav = $('.mobile-nav-home');
+		
+		$('.mobile-nav-toggle').on('click', function(){
+			$mobileNav.toggleClass('active');
+		});
 	});
-}
+})( jQuery );
 
-function nodeJump(){
-    $('body').on('mouseenter', '.outer-node, .return-node', 
-        function(){
-            $(this).removeClass('out').addClass('in');
-        }
-    );
-    
-    $('body').on('mouseleave', '.outer-node, .return-node', 
-        function(){
-            $(this).removeClass('in').addClass('out');
-        }
-    );
-}
+//====================================================================================================================//
 
-function contentLoad($passedThis){
+/*------------------------------
+		Smooth Scrolling
+------------------------------*/
 
-    $.ajaxSetup({ cache: false });
-    
-    var $newFolder = $passedThis.data('folder'),
-        $newFile = $newFolder + '/' + $passedThis.data('file');
-    
-    function nodeExpand(){
-        $('.outer-node, .top-nodes-wrap, .bottom-node-wrap, .top-connectors-wrap, .top-connector, .bottom-connector').removeClass('retracted');
-    }
-        
-    $('.work-wrap').load($newFile);
-    $( document ).ajaxSuccess(function(){
-        setTimeout(function(){nodeExpand()}, 500);
-    });
-}
+(function( $ ){
+	$(document).ready(function(){
+		$('a[href^="#"]').on('click', function(event){
+			
+			var $this = $(this),
+				$target = $($this.attr('href')),
+				$html = $('html, body'),
+				
+				$scrollTop = $(window).scrollTop(),
+				sectionOffset = $target.parent().offset().top,
+				
+				distance = sectionOffset - scrollTop,
+				duration = distance / scrollSpeed ;
+			
 
-function nodeRetract(){
-    $('body').on('click', '.outer-node, .return-node', function(){
-        
-        var $this = $(this),
-            $outerNode = $('.outer-node'),
-            $returnNode = $('.return-node'),
-            $wrapsAndConnectors = $('.top-nodes-wrap, .bottom-node-wrap, .top-connectors-wrap, .top-connector, .bottom-connector'),
-            $allNodes = $('.outer-node, .return-node');
-        
-        $outerNode.toggleClass('retracted');
-        $wrapsAndConnectors.toggleClass('retracted');
-        $allNodes.removeClass('selected-node');
-        $this.addClass('selected-node');
-        
-        setTimeout(function(){contentLoad($this)}, 500);
-    });
-}
+			if ($target.length > 0) {
+				event.preventDefault();
+				$html.animate({
+					scrollTop: sectionOffset
+				}, duration);
+			}
+		});
+	});
+})( jQuery );
+
+//====================================================================================================================//
+
+/*------------------------------
+	Node Pop-In and Pop-Out
+------------------------------*/
+
+(function( $ ){
+	$(document).ready(function(){
+		$('body').on('mouseenter', '.outer-node, .return-node', 
+			function(){
+				$(this).removeClass('out').addClass('in');
+			}
+		);
+		
+		$('body').on('mouseleave', '.outer-node, .return-node', 
+			function(){
+				$(this).removeClass('in').addClass('out');
+			}
+		);
+	});
+})( jQuery );
+
+//====================================================================================================================//
+
+/*------------------------------
+		Node Retraction
+------------------------------*/
+(function( $ ) {
+	$(document).ready(function(){
+		$('body').on('click', '.outer-node, .return-node', function(){
+			
+			var $this = $(this),
+				$outerNode = $('.outer-node'),
+				$returnNode = $('.return-node'),
+				$wrapsAndConnectors = $('.top-nodes-wrap, .bottom-node-wrap, .top-connectors-wrap, .top-connector, .bottom-connector'),
+				$allNodes = $('.outer-node, .return-node');
+			
+			$outerNode.toggleClass('retracted');
+			$wrapsAndConnectors.toggleClass('retracted');
+			$allNodes.removeClass('selected-node');
+			$this.addClass('selected-node');
+			
+			setTimeout(function(){contentLoad($this)}, 500);
+		});
+		
+		function contentLoad($passedThis){
+
+			$.ajaxSetup({ cache: false });
+			
+			var $newFolder = $passedThis.data('folder'),
+				$newFile = $newFolder + '/' + $passedThis.data('file');
+			
+			function nodeExpand(){
+				$('.outer-node, .top-nodes-wrap, .bottom-node-wrap, .top-connectors-wrap, .top-connector, .bottom-connector').removeClass('retracted');
+			}
+				
+			$('.work-wrap').load($newFile);
+			$( document ).ajaxSuccess(function(){
+				setTimeout(function(){nodeExpand()}, 500);
+			});
+		}
+	
+	});
+})( jQuery );
 
 //==================================================================================================================================//
 
